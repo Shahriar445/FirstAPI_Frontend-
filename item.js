ef2c4870bbcc6d08d3daa5d1d5de8654390a65fd
Shahrriar Haque <shahriarhaque445@gmail.com>
2024-06-05 11:55:47 +0600

    item list show

--- item.js
+++ item.js
@@ -1,3 +1,11 @@
+// for show all list items 
+document.addEventListener('DOMContentLoaded',async function(){
+    try{
+        await DisplayItemList();
+    }catch(error){
+        console.error('error items',error);
+    }
+});
 
 
 document.getElementById('itemForm').addEventListener('submit',handleSubmit);
@@ -10,6 +18,7 @@ async function handleSubmit(event){
         const response =await sendItemData(itemData);
         const result = await response.text();
         alert(result);
+        await DisplayItemList();
     }catch(error)
     {
         console.error('Error',error);
@@ -34,4 +43,34 @@ async function sendItemData(itemData)
         },
         body:JSON.stringify(itemData)
     });
+}
+
+async function DisplayItemList()
+{
+    try{
+        const response = await fetch('https://localhost:7242/GetAllItems'  );
+        if (!response.ok){
+            throw new error('network response want not ok');
+        }
+
+        const items = await response.json();
+        const tableBody = document.querySelector('#itemTable tbody');
+
+        tableBody.innerHTML='';
+        items.forEach(item=>{
+           const row = tableBody.insertRow();
+
+            row.innerHTML= `
+            <td>${item.itemId}</td>
+            <td>${item.itemName}</td>
+            <td> ${item.numStockQuantity}</td>
+            <td> ${item.isActive ? 'Yes':'No'}</td>
+            `;
+            
+        });
+
+    }
+    catch(error){
+        console.error('error for item list',error);
+    }
 }
